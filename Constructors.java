3543bbcee9936f15c6273c81b8d45818c044cdd1
cmirkhad <60456879+cmirkhad@users.noreply.github.com>
2020-02-17 12:54:06 +0600

    Update Constructors.java

--- Constructors.java
+++ Constructors.java
@@ -4,27 +4,71 @@ import java.util.ArrayList;
 
 public class Constructors {
     private String input;
+    private String result;
+    private int pseudocity;
+    private TextList list;
+    ArrayList<String> usercities = new ArrayList<>();
+    public Constructors() {
+        list = new TextList();
+        list.show();
 
-    public Constructors(String input) {
-        this.input=input;
+
     }
 
-    public String getInput() {
-        return input;
+    public String getResult() {
+        return result;
     }
-    ArrayList<String> bothinput= new ArrayList<String>();
-    public void setInput(String input) {
-        this.input = input;
-        if (input.length()>20){
-            System.out.println("Incorrect city!");
+    public void setInput(String inut) {
+        this.input=inut;
+        pseudocity=0;
+        checkforvalidcity();
+
+        checkforused();
+
+
+
+        if (pseudocity==0){
+           usercities.add(input);
+           generatenewcity();
+           usercities.add(result);
+            System.out.println(getResult());
         }
 
-        if (bothinput.contains(input)){
-            System.out.println("It was used!");
-        }else{
-
-            bothinput.add(input);}
-
-        System.out.println(bothinput);
     }
+    private void checkforvalidcity(){
+        if (result!=null){
+        if ((result.charAt(result.length()-1)&65503)!=input.charAt(0)){
+            System.out.println("Set city begins with "+(char) (result.charAt(result.length()-1)&65503));
+            pseudocity+=1;
+        }}
+
+    }
+    private void checkforused(){
+        if (usercities.size()>0){
+            for (String citycheck: usercities){
+                if (citycheck.equals(input)){
+                    System.out.println("It was used. Try again!");
+                    pseudocity+=1;
+                    break;
+
+                }
+            }}
+    }
+    private void generatenewcity(){
+        for (int i =0; i<list.copylist.size(); i++){
+            if (list.copylist.get(i).charAt(0)==(input.charAt(input.length()-1) & 65503)){
+                result=list.copylist.get(i);
+                list.copylist.remove(i);
+                if (list.copylist.indexOf(input)!=-1){
+                    list.copylist.remove(input);
+                }
+                break;
+            }
+        }
+    }
+    public void usercitiesinfo(){
+
+        System.out.println(usercities);
+    }
+
 }
